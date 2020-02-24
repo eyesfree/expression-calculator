189e52bbd9957ee17021e02d5f23aa57373163ef
eyesfree <kristinavancappelle@protonmail.com>
2020-02-24 18:41:15 +0100

    remove unit.js

--- tests/test.js
+++ tests/test.js
@@ -1,48 +1,50 @@
 'use strict';
 
-const supertest = require('supertest'); 
-const test = require('unit.js');
+const supertest = require('supertest');
 const app = require('../app.js');
 
 const request = supertest(app);
 
 describe('Tests app', function() {
 
-  it('verify calculate', function(done) {
-    request.get('/calculus?query=MTEgKyAzICogMTAgLTQ=').expect(200).end(function(err, result) {
-        test.object(result.body).contains({error: 'false', result: '37'});
-        done(err);
+  it('verify calculate', function() {
+    return supertest(app)
+              .get('/calculus?query=MTEgKyAzICogMTAgLTQ=')
+              .expect(200)
+              .expect('Content-Type',/json/)
+              .expect({error: 'false', result: '37'});
     });
+
+  it('verify equation with parenthesis', function() {
+    return supertest(app)
+              .get('/calculus?query=MiAqICgyMy8oMyozKSktIDIzICogKDIqMyk')
+              .expect(200)
+              .expect('Content-Type',/json/)
+              .expect({error: 'false', result: '-132.88888888888889'});
   });
 
-  
-
-  it('verify equation with parenthesis', function(done) {
-    request.get('/calculus?query=MiAqICgyMy8oMyozKSktIDIzICogKDIqMyk').expect(200).end(function(err, result) {
-        test.object(result.body).contains({error: 'false', result: '-132.88888888888889'});
-        done(err);
-    });
+  it('negative calculate', function() {
+    return supertest(app)
+              .get('/calculus?query=NSArICgxMjAqNDY3KjIgLTU=')
+              .expect(400)
+              .expect('Content-Type',/json/)
+              .expect({error: 'true', message:'Calculation error: SyntaxError: Parenthesis ) expected (char 18)'});
   });
 
-  it('negative calculate', function(done) {
-    request.get('/calculus?query=NSArICgxMjAqNDY3KjIgLTU=').expect(400).end(function(err, result) {
-        test.string(result.body.message).contains('SyntaxError: Parenthesis ) expected (char 18)');
-        done(err);
-    });
+  it('no input parameter', function() {
+    return supertest(app)
+              .get('/calculus')
+              .expect(400)
+              .expect('Content-Type',/json/)
+              .expect({error: 'true', message: 'No query parameter set.'});
   });
 
-  it('no input parameter', function(done) {
-    request.get('/calculus').expect(400).end(function(err, result) {
-        test.string(result.body.message).contains('No query parameter');
-        done(err);
-    });
-  });
-
-  it('invalid input test', function(done) {
-    request.get('/calculus?query==').expect(400).end(function(err, result) {
-        test.string(result.body.message).contains('Invalid expression');
-        done(err);
-    });
+  it('invalid input test', function() {
+    return supertest(app)
+              .get('/calculus?query==')
+              .expect(400)
+              .expect('Content-Type',/json/)
+              .expect({error: 'true', message:'Calculation error: Invalid expression passed as query.'});
   });
 
 });
